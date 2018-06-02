81f27cbb307e4102b60b5b7ae89bab205c6a8ca6
BhuvaneswariRamasamy <bhuvanakeerthi1197@gmail.com>
2018-06-02 16:04:33 +0530

    To print student details

---
+++ src/com/student/Students.java
@@ -0,0 +1,55 @@
+package com.student;
+
+public class Students {
+	private int studentId;
+	private String firstName;
+	private String lastName;
+	private String branch;
+	private String gender;
+	
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return studentId+"\t"+firstName+"\t"+lastName+"\t"+branch+"\t"+gender;
+	}
+}
